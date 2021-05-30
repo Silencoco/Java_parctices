05951ca62079cbc8ed14a9bfb241eb16bb78bf1c
Silencoco <1187742621@qq.com>
2021-05-30 16:29:55 +0800

    第一次上传

---
+++ src/com/silencoco/Lottery.java
@@ -0,0 +1,30 @@
+package com.silencoco;
+
+import java.util.*;
+
+/**
+ * 字符串随机点名且不重复
+ *     定义一个字符串数组用来装源数据
+ *     定义一个字符串用来装随机后的字符串
+ *     通过随机索引随机字符串
+ *     每次随机后删除随机的那个字符串
+ *     最后为了防止随机数为0报异常写一个判断语句
+ */
+public class Lottery {
+    public static void main(String[] args){
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list,"你好","我好","大家好","good","better","very good");
+        String list1 = new String();
+        Random random = new Random();
+        while (list1!=null){
+            int r = random.nextInt(list.size());
+            list1 = list.get(r);
+            list.remove(r);
+            System.out.println(list1);
+            //随机数不能
+            if (r==0){
+                break;
+            }
+        }
+    }
+}
---
+++ src/com/silencoco/_NET/TCPServer.java
@@ -0,0 +1,33 @@
+package com.silencoco._NET;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 创建ServerSocket对象，指定端口号，
+ * 使用ServerSocket对象中的accept获取到请求的客户端对象Socket
+ * 使用socket对象中国年的getInputStream获取输入流
+ * read读取
+ * 在用socket.getOutputStream write 回写数据
+ * close
+ */
+public class TCPServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8888);
+        Socket socket = serverSocket.accept();
+        //读取客户端的数据
+        InputStream is = socket.getInputStream();
+        byte[] bytes = new byte[1024];
+        int len = is.read(bytes);
+        System.out.printf(new String(bytes,0,len));
+        //向客户端回写数据
+        OutputStream os = socket.getOutputStream();
+        os.write("服务器收到请求并响应".getBytes());
+
+        serverSocket.close();
+        socket.close();
+    }
+}
